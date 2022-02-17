fbb5a467112c7c60c29ad960283614ee381dd98f
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2022-02-17 15:59:10 +0600

    Added required element

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+/* Inputs */
+const incomeInput = document.getElementById('income');
+const foodInput = document.getElementById('food');
+const rentInput = document.getElementById('rent');
+const clothesInput = document.getElementById('clothes');
+const percentageInput = document.getElementById('percentage');
+
+/* Action buttons */
+const calcExpenseBtn = document.getElementById('calc-expenses');
+const calcSavingsBtn = document.getElementById('calc-savings');
+
+/* Displays */
+const expenseDisplay = document.getElementById('expense-display');
+const balanceDisplay = document.getElementById('balance-display');
+
+/* Functions */
