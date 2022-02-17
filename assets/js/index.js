685b733d595603b4f14a1e36530e4e7f9ce5521f
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2022-02-17 16:12:20 +0600

    Added calculateExpenseAndBalance and calculateSavings functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,5 +12,48 @@ const calcSavingsBtn = document.getElementById('calc-savings');
 /* Displays */
 const expenseDisplay = document.getElementById('expense-display');
 const balanceDisplay = document.getElementById('balance-display');
+const savingAmountDisplay = document.getElementById('saving-amount');
+const remainingBalanceDisplay = document.getElementById('remaining-balance');
 
 /* Functions */
+
+/** 
+ * Calculate expenses and display expenses and remaining balance.
+ * @return {void}
+*/
+function calculateExpenseAndBalance() {
+    const income = parseFloat(incomeInput.value);
+    const food = parseFloat(foodInput.value);
+    const rent = parseFloat(rentInput.value);
+    const clothes = parseFloat(clothesInput.value);
+    
+    const totalExpense = food + rent + clothes;
+
+    if ( totalExpense > income ) {
+        alert('You have spent more than your income!');
+    }
+
+    const result = income - totalExpense;
+
+    expenseDisplay.innerText = totalExpense;
+    balanceDisplay.innerText = result;
+}
+
+/** 
+ * Calculate savings and display Savings amount and remaining balance.
+ * @return {void}
+*/
+function calculateSavings() {
+    const currentBalance = parseFloat(balanceDisplay.innerText);
+    const percentage = parseFloat(percentageInput.value);
+
+    const savingAmount = currentBalance * (percentage / 100);
+    const remainingBalance = currentBalance - savingAmount;
+
+    savingAmountDisplay.innerText = savingAmount;
+    remainingBalanceDisplay.innerText = remainingBalance;
+}
+
+/* Event listeners */
+calcExpenseBtn.addEventListener('click', calculateExpenseAndBalance);
+calcSavingsBtn.addEventListener('click', calculateSavings);
